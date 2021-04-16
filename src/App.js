f871850e4687ddd9d60e8ced68b426e367da37cb
Kosmostars7403 <64686091+Kosmostars7403@users.noreply.github.com>
2021-04-16 10:37:57 +0300

    Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import './App.scss';
+import Car from './Car/Car'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends  Component {
+
+  state = {
+    cars: [
+      {brand: 'KIA', year: '2020'},
+      {brand: 'MERSEDES', year: '2012'},
+      {brand: 'FORD', year: '2008'},
+      {brand: 'BMW', year: '2021'},
+    ],
+    pageTitle: 'React Study!',
+    showCars: true
+  }
+
+  changeTitle = (title) => {
+    this.setState({
+      pageTitle: title
+    })
+  }
+
+  handleInput = (event) => {
+    this.setState({
+      pageTitle: event.target.value
+    })
+  }
+
+  toggleCars = () => this.setState({showCars: !this.state.showCars})
+
+  changeCarName(name, index) {
+    const cars = [...this.state.cars]
+    cars[index].brand = name
+    this.setState({cars})
+  }
+
+  delete(index) {
+    const cars = [...this.state.cars]
+    cars.splice(index,1)
+    this.setState({cars})
+  }
+
+  render() {
+    const divStyle = {
+      textAlign: 'center',
+    }
+
+    let cars = null
+
+    if (this.state.showCars) {
+      cars = this.state.cars.map((car, index) => {
+        return (
+          <Car
+            key={index}
+            brand={car.brand}
+            year={car.year}
+            onChangeTitle={() => this.changeTitle(car.brand)}
+            onChangeCar={() => this.changeCarName('pew', index)}
+            onCarDelete={this.delete.bind(this, index)}
+          >
+            <p>pew</p>
+          </Car>
+        )
+      })
+    }
+
+    return (
+      <div style={divStyle}>
+        {/*<h1>{this.state.pageTitle}</h1>*/}
+        <h1>{this.props.title}</h1>
+        <input onChange={this.handleInput} />
+        <button onClick={this.toggleCars}>toggleCars</button>
+        { cars }
+      </div>
+    )
+  }
 }
+
 
 export default App;
